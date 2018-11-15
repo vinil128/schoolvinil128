362537697b83f0ce900d0fb599a0f45bf62a3185
Егор <vinil128@yahoo.com>
2018-11-15 12:48:56 +0200

    init yii framework minimal

--- web/index.php
+++ web/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo '<h1>Video School</h1>';
+    define('YII_DEBUG', true);
+    require             (__DIR__ . '/../vendor/autoload.php');
+    require             (__DIR__ . '/../vendor/yiisoft/yii2/yii.php');
+    $config = require   (__DIR__ . '/../config/web.php');
+    $app = new yii\web\Application($config);
+    $app->run();
+
+?>
